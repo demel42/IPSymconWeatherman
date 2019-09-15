d1bb177463668ad8721245f7d3a4ae3b723becbb
demel42 <demel42@web.de>
2019-09-15 11:15:57 +0200

    first commit

---
+++ Weatherman/module.php
@@ -0,0 +1,69 @@
+<?php
+
+require_once __DIR__ . '/../libs/common.php';  // globale Funktionen
+
+class Weatherman extends IPSModule
+{
+    use WeathermanCommon;
+
+    public function Create()
+    {
+        parent::Create();
+
+        $this->RequireParent('{8062CF2B-600E-41D6-AD4B-1BA66C32D6ED}');
+    }
+
+    public function ApplyChanges()
+    {
+        parent::ApplyChanges();
+
+        $vpos = 1;
+
+        $this->SetStatus(IS_ACTIVE);
+    }
+
+    public function GetConfigurationForm()
+    {
+        $formElements = $this->GetFormElements();
+        $formActions = $this->GetFormActions();
+        $formStatus = $this->GetFormStatus();
+
+        $form = json_encode(['elements' => $formElements, 'actions' => $formActions, 'status' => $formStatus]);
+        if ($form == '') {
+            $this->SendDebug(__FUNCTION__, 'json_error=' . json_last_error_msg(), 0);
+            $this->SendDebug(__FUNCTION__, '=> formElements=' . print_r($formElements, true), 0);
+            $this->SendDebug(__FUNCTION__, '=> formActions=' . print_r($formActions, true), 0);
+            $this->SendDebug(__FUNCTION__, '=> formStatus=' . print_r($formStatus, true), 0);
+        }
+        return $form;
+    }
+
+    protected function GetFormElements()
+    {
+        $formElements = [];
+
+        return $formElements;
+    }
+
+    protected function GetFormActions()
+    {
+        $formActions = [];
+        $formActions[] = ['type' => 'Button', 'label' => 'Update data', 'onClick' => 'WeathermanDevice_UpdateData($id);'];
+        $formActions[] = ['type' => 'Label', 'label' => '____________________________________________________________________________________________________'];
+        $formActions[] = [
+                            'type'    => 'Button',
+                            'caption' => 'Module description',
+                            'onClick' => 'echo "https://github.com/demel42/IPSymconWeatherman/blob/master/README.md";'
+                        ];
+
+        return json_encode(['elements' => $formElements, 'actions' => $formActions, 'status' => $formStatus]);
+    }
+
+	public function ReceiveData($data)
+	{
+		$jdata = json_decode($data, true);
+		$buf = utf8_decode($jdata['Buffer']);
+
+		$this->SendDebug(__FUNCTION__, 'buf=' . $buf, 0);
+	}
+}
